48a87c43db6adcd3fa926ac2c1a646b91a793214
daniel lucana cayo <daniellucanacayo@gmail.com>
2023-12-20 14:14:25 -0400

    Agregar archivos de proyecto.

---
+++ LaptoManiaOficial/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LaptoManiaOficial.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
